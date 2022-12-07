c991ef95c41ce5f8bf9aa126882865ccaec4495b
Ron <rc72095@icloud.com>
2022-12-06 20:52:27 -0500

    adding 2 solutions to Palindrome problem

--- 9.Palindrome_Number/Palindrome_Number.js
+++ 9.Palindrome_Number/Palindrome_Number.js
@@ -1,5 +1,7 @@
+//convert to string approach to treat input as an array
 const test = '121';
 const test2 = '3567';
+const test3 = 353;
 
 function isPalindrome(x) {
   let xString = x.toString();
@@ -10,3 +12,26 @@ function isPalindrome(x) {
 
 console.log(isPalindrome(test));
 console.log(isPalindrome(test2));
+console.log(isPalindrome(test3));
+
+//number approach by manipulating the digit in the tens place
+//no converting input to string
+
+function isPalindrome2(x){
+  if(x < 0) return false
+  if(x % 10 === 0 && x !== 0) return false
+
+  const temp = x;
+  let reverseNumber = 0;
+
+  while(x>0){
+    reverseNumber = (reverseNumber * 10) + (x%10)
+    x = parseInt(x/10);
+  }
+
+  return temp == reverseNumber;
+}
+
+console.log(isPalindrome2(test));
+console.log(isPalindrome2(test2));
+console.log(isPalindrome2(test3));
