c78db36b3886d620a6f2f9305a4f5b963c93fda2
Ron <rc72095@icloud.com>
2022-12-12 19:47:25 -0500

    workiong on common prefix solution

--- 14.Longest_Common_Prefix/Longest_Common_Prefix.js
+++ 14.Longest_Common_Prefix/Longest_Common_Prefix.js
@@ -1,5 +1,9 @@
 const words = ['splash', 'spleen', 'split'];
 
-function displayCommonPrefix(list){
-  
+function displayCommonPrefix(listArray){
+  const firstWord = listArray[0];
+
+  for(let i = 0; i < listArray.length; i++){
+    
+  }
 }
