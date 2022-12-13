2b01d1e71b38dfaad520ca2f9ad29a61769b9211
Ron <rc72095@icloud.com>
2022-12-12 20:01:46 -0500

    solved common prefix & added commenting

--- 14.Longest_Common_Prefix/Longest_Common_Prefix.js
+++ 14.Longest_Common_Prefix/Longest_Common_Prefix.js
@@ -1,9 +1,17 @@
 const words = ['splash', 'spleen', 'split'];
 
 function displayCommonPrefix(listArray){
-  const firstWord = listArray[0];
+  if(strs === null || strs.length == 0) return ""; //return empty string if no data or an empty string was inputted
 
-  for(let i = 0; i < listArray.length; i++){
-    
+  let prefix = ''; //intialize a variable to store the common prefix;
+
+  for(let i = 0; i < listArray[0].length; i++){ //loop over the first word to check against others
+    const firstWord = listArray[0][i];
+
+    for(let j = 1; j < listArray.length; j++){ //start at index 1 and loop over each character of words
+      if(listArray[j][i] !== firstWord) return prefix; //return prefix once a letter does not match the letter in the 1st word at the same index
+    }
+    prefix = prefix + firstWord; //update the common prefix variable upon each successful loop 
   }
+  return prefix;
 }
