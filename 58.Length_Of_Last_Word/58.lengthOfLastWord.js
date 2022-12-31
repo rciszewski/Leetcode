f8b10d5e524e7b7ce680fbbfb166e78e6af47005
Ron <rc72095@icloud.com>
2022-12-31 11:09:36 -0500

    add solution to challenge 58

---
+++ 58.Length_Of_Last_Word/58.lengthOfLastWord.js
@@ -0,0 +1,17 @@
+var lengthOfLastWord = function(s) {
+  let lastWord = '';
+  let length = 0;
+  const sArray = s.split(' ');
+  console.log(sArray)
+  
+  for(i = sArray.length; i >= 0; i--){
+    if(typeof sArray[i] === "string" && sArray[i].length > 0){
+      lastWord = sArray[i];
+      length = sArray[i].length;
+      return `The last word is ${lastWord} with a length of ${length} letters`;
+    } 
+  }
+  return `There are no words inputted`;
+};
+
+console.log(lengthOfLastWord("Hello   World   "))
